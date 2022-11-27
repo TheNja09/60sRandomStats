cf52ddc3e595e59bcef0af6ed9cbda9b13b62267
TheNja09 <98636004+TheNja09@users.noreply.github.com>
2022-11-26 20:59:19 -0600

    Update randomstats.lua

--- randomstats.lua
+++ randomstats.lua
@@ -1,4 +1,5 @@
-rngWait = 3600
+rngWait = 3600 --This is the time between random stat generations, counted in frames (3600 equals 1 minute)
+--You can ignore these values below
 rngBaseSpeed = 8
 rngValor = 12
 rngWisdom = 12
@@ -29,6 +30,8 @@ rngFinalCost = 5
 rngSummonCost = 3
 rngMagicCost = 15
 rngQuickRun = 30
+rngADodgeHeight = 145
+rngADodgeSpeed = 18
 function _OnFrame()
     World = ReadByte(Now + 0x00)
     Room = ReadByte(Now + 0x01)
@@ -77,37 +80,42 @@ function Cheats()
     if ReadByte(Slot1+0x180) > ReadByte(Slot1+0x184) then
         WriteByte(Slot1+0x180, ReadByte(Slot1+0x184))
     end
+    -- Okay this is where the fun stuff is!
+    -- If you would like to edit the minimum and maximum RNG values, just change the values in math.random().
+    -- The first number is the minimum value, and the second number is the maximum value.
     if rngWait == 0 then
-        rngBaseSpeed = math.random(1,40)
-        rngValor = math.random(1,60)
-        rngWisdom = math.random(1,60)
-        rngMaster = math.random(1,50)
-        rngFinal = math.random(1,80)
-        rngAnti = math.random(1,80)
-        rngLion = math.random(1,90)
-        rngMermaid = math.random(1,35)
-        rngCarpet = math.random(1,100)
-        rngDice = math.random(1,40)
-        rngCard = math.random(1,40)
-        rngLimit = math.random(1,40)
-        rngAttack = math.random(1,80)
-        rngMagic = math.random(1,80)
-        rngDefense = math.random(1,80)
-        rngHP = math.random(1,120)
-        rngMP = math.random(1,140)
-        rngGlideSpeed = math.random(1,100)
-        rngScale = math.random(10,100)/100
-        rngAnimation = math.random(3,50)/10
-        rngHighJump = math.random(1,2000)
-        rngAerial = math.random(1,100)
-        rngValorCost = math.random(0,9)
-        rngWisdomCost = math.random(0,9)
-        rngLimitCost = math.random(0,9)
-        rngMasterCost = math.random(0,9)
-        rngFinalCost = math.random(0,9)
-        rngSummonCost = math.random(0,9)
-        rngMagicCost = math.random(0,140)
-        rngQuickRun = math.random(1,125)
+        rngBaseSpeed = math.random(1,40) -- Base running speed of Sora/Roxas. The minimum is 1, and the maxmimum is 40.
+        rngValor = math.random(1,60) -- Base running speed of Valor. Min is 1, Max is 60, and so on.
+        rngWisdom = math.random(1,60) -- Base Wisdom Speed
+        rngMaster = math.random(1,50) -- Base Master Speed
+        rngFinal = math.random(1,80) -- Base Final Speed
+        rngAnti = math.random(1,80) -- Base Anti Speed
+        rngLion = math.random(1,90) -- Base Lion Speed (Does not include dash)
+        rngMermaid = math.random(1,35) -- Base Mermaid Speed
+        rngCarpet = math.random(1,100) -- Base Carpet Speed
+        rngDice = math.random(1,40) -- Base Dice Speed
+        rngCard = math.random(1,40) -- Base Card Speed (Does not seem to work?)
+        rngLimit = math.random(1,40) -- Base Limit Form Speed
+        rngAttack = math.random(1,80) -- Attack Stat (Note: for ATK, MAG, & DEF, leveling up and stat boosts will not work with this mod.)
+        rngMagic = math.random(1,80) -- Magic Stat
+        rngDefense = math.random(1,80) -- Defense Stat
+        rngHP = math.random(1,120) -- Maximum HP
+        rngMP = math.random(1,140) -- Maximum MP
+        rngGlideSpeed = math.random(1,100) -- Glide Speed (Affects all levels with the same value)
+        rngScale = math.random(10,300)/100 -- Sora's Size (Divided by 100 in order to get decimal values)
+        rngAnimation = math.random(3,50)/10 -- Sora's Animation Speed
+        rngHighJump = math.random(1,2000) -- Jump Height (Affects base and all High Jump levels)
+        rngAerial = math.random(1,100) -- Aerial Recovery Movement Speed
+        rngValorCost = math.random(0,9) -- Valor Form Cost
+        rngWisdomCost = math.random(0,9) -- Wisdom Form Cost
+        rngLimitCost = math.random(0,9) -- Limit Form Cost
+        rngMasterCost = math.random(0,9) -- Master Form Cost
+        rngFinalCost = math.random(0,9) -- Final Form Cost
+        rngSummonCost = math.random(0,9) -- Summon Costs (Affects all summons)
+        rngMagicCost = math.random(0,140) -- Magic Cost (Affects every magic spell and limit)
+        rngQuickRun = math.random(1,125)  -- Quick Run Speed
+        rngADodgeHeight = math.random(0,2000)  -- Aerial Dodge Height (Affects all levels)
+        rngADodgeSpeed = math.random(1,350) -- Aerial Dodge Speed [Horizontal Speed] (Affects all levels)
     end
     WriteFloat(Sys3+0x17CE4, rngBaseSpeed) -- Base Speed: DS = 8
     WriteFloat(Sys3+0x17D18, rngValor) -- Valor Form: DS = 12
@@ -121,22 +129,22 @@ function Cheats()
     WriteFloat(Sys3+0x181F8, rngDice) -- Dice Sora: DS = 8
     WriteFloat(Sys3+0x1822C, rngCard) -- Card Sora: DS = 8
     WriteFloat(Sys3+0x18364, rngLimit) -- Limit Form: DS = 8
-    WriteByte(Slot1+0x4, rngHP)
-    WriteByte(Slot1+0x184, rngMP)
-    WriteByte(0x24BC8D2, rngAttack)
-    WriteByte(0x24BC8D4, rngMagic)
-    WriteByte(0x24BC8D6, rngDefense)
-    WriteFloat(0x250D332, rngGlideSpeed) --Glide 1 Speed (Default: 16)
-    WriteFloat(0x250D376, rngGlideSpeed) --Glide 2 Speed (Default: 20)
-    WriteFloat(0x250D3BA, rngGlideSpeed) --Glide 3 Speed (Default: 24)
-    WriteFloat(0x250D3FE, rngGlideSpeed) --Glide MAX Speed (Default: 32)
-    WriteFloat(0x250D442, rngGlideSpeed) --Glide AX2 Speed (Default: 64)
-    WriteFloat(0x250D312, rngHighJump)
+    WriteByte(Slot1+0x4, rngHP) -- Max HP
+    WriteByte(Slot1+0x184, rngMP) -- Max MP
+    WriteByte(0x24BC8D2, rngAttack) -- Attack Stat
+    WriteByte(0x24BC8D4, rngMagic) -- Magic Stat
+    WriteByte(0x24BC8D6, rngDefense) -- Defense Stat
+    WriteFloat(0x250D332, rngGlideSpeed) -- Glide 1 Speed (Default: 16)
+    WriteFloat(0x250D376, rngGlideSpeed) -- Glide 2 Speed (Default: 20)
+    WriteFloat(0x250D3BA, rngGlideSpeed) -- Glide 3 Speed (Default: 24)
+    WriteFloat(0x250D3FE, rngGlideSpeed) -- Glide MAX Speed (Default: 32)
+    WriteFloat(0x250D442, rngGlideSpeed) -- Glide AX2 Speed (Default: 64)
+    WriteFloat(0x250D312, rngHighJump) -- Sora Base Jump Height
     WriteFloat(0x250D356, rngHighJump) -- Sora High Jump 2
     WriteFloat(0x250D39A, rngHighJump) -- Sora High Jump 3
     WriteFloat(0x250D3DE, rngHighJump) -- Sora High Jump MAX
     WriteFloat(0x250D422, rngHighJump) -- Sora High Jump AX2
-    WriteFloat(0x250CEC6, rngAerial)
+    WriteFloat(0x250CEC6, rngAerial) -- Aerial Recovery Movement Speed
     WriteByte(Sys3+0x03E0,rngValorCost) -- Valor
     WriteByte(Sys3+0x0410,rngWisdomCost) -- Wisdom
     WriteByte(Sys3+0x7A30,rngLimitCost) -- Limit
@@ -146,56 +154,67 @@ function Cheats()
     WriteByte(Sys3+0x1070,rngSummonCost) -- Stitch
     WriteByte(Sys3+0x10A0,rngSummonCost) -- Genie
     WriteByte(Sys3+0x37A0,rngSummonCost) -- Pan
-    WriteByte(Sys3+0x9E0,rngMagicCost) -- Fire Cost: 15
-    WriteByte(Sys3+0x15E0,rngMagicCost) -- Fira Cost: 15
-    WriteByte(Sys3+0x1610,rngMagicCost) -- Firaga Cost: 15
-    WriteByte(Sys3+0xA40,rngMagicCost) -- Blizzard Cost: 15
-    WriteByte(Sys3+0x1640,rngMagicCost) -- Blizzara Cost: 15
-    WriteByte(Sys3+0x1670,rngMagicCost) -- Blizzaga Cost: 15
-    WriteByte(Sys3+0xA10,rngMagicCost) -- Thunder Cost: 15
-    WriteByte(Sys3+0x16A0,rngMagicCost) -- Thundara Cost: 15
-    WriteByte(Sys3+0x16D0,rngMagicCost) -- Thundaga Cost: 15
-    WriteByte(Sys3+0xA70,rngMagicCost) -- Cure Cost: 3
-    WriteByte(Sys3+0x1700,rngMagicCost) -- Cura Cost: 2
-    WriteByte(Sys3+0x1730,rngMagicCost) -- Curaga Cost: 2
-    WriteByte(Sys3+0x1F40,rngMagicCost) -- Magnet Cost: 15
-    WriteByte(Sys3+0x1F70,rngMagicCost) -- Magnera Cost: 15
-    WriteByte(Sys3+0x1FA0,rngMagicCost) -- Magnega Cost: 15
-    WriteByte(Sys3+0x1FD0,rngMagicCost) -- Reflect Cost: 3
-    WriteByte(Sys3+0x2000,rngMagicCost) -- Reflera Cost: 2
-    WriteByte(Sys3+0x2030,rngMagicCost) -- Reflega Cost: 1
-    WriteByte(Sys3+0xE30,rngMagicCost) -- Twin Howl Cost: 254
-    WriteByte(Sys3+0xFB0,rngMagicCost) -- Bushido Cost: 254
-    WriteByte(Sys3+0x1940,rngMagicCost) -- Red Rocket Cost: 254
-    WriteByte(Sys3+0x3F80,rngMagicCost) -- Speedster Cost: 254
-    WriteByte(Sys3+0x40A0,rngMagicCost) -- Bluff Cost: 254
-    WriteByte(Sys3+0x4430,rngMagicCost) -- Wildcat Cost: 254
-    WriteByte(Sys3+0x49A0,rngMagicCost) -- Dance Call Cost: 254
-    WriteByte(Sys3+0x4B80,rngMagicCost) -- Setup Cost: 254
-    WriteByte(Sys3+0x67D0,rngMagicCost) -- Session Cost: 254
-    WriteByte(Sys3+0x5840,rngMagicCost) -- Trinity Limit Cost: 254
-    WriteByte(Sys3+0x5840+0x1830,rngMagicCost) -- (Solo) Trinity Limit Cost: 3
-    WriteByte(Sys3+0x2E10,rngMagicCost) -- Whirli-Goof Cost: 3
-    WriteByte(Sys3+0x3D40,rngMagicCost) -- Knocksmash Cost: 3
-    WriteByte(Sys3+0x3320,rngMagicCost) -- Comet Cost: 254
-    WriteByte(Sys3+0x3E60,rngMagicCost) -- Duck Flare Cost: 254
-    WriteByte(Sys3+0x7E50,rngMagicCost) -- Strike Raid Cost: 1
-    WriteByte(Sys3+0x7D30,rngMagicCost) -- Sonic Blade Cost: 2
-    WriteByte(Sys3+0x7C10,rngMagicCost) -- Ragnarok Cost: 100
-    WriteByte(Sys3+0x7DC0,rngMagicCost) -- Ars Arcanum Cost: 3
-    WriteFloat(0x250D322, rngQuickRun) --QR1 Speed
-    WriteFloat(0x250D366, rngQuickRun) --QR2 Speed
-    WriteFloat(0x250D3AA, rngQuickRun) --QR3 Speed
-    WriteFloat(0x250D3EE, rngQuickRun) --QR4 Speed
-    WriteFloat(0x250D432, rngQuickRun) --AX2 QR Speed
+    WriteByte(Sys3+0x9E0,rngMagicCost) -- Fire Cost
+    WriteByte(Sys3+0x15E0,rngMagicCost) -- Fira Cost
+    WriteByte(Sys3+0x1610,rngMagicCost) -- Firaga Cost
+    WriteByte(Sys3+0xA40,rngMagicCost) -- Blizzard Cost
+    WriteByte(Sys3+0x1640,rngMagicCost) -- Blizzara Cost
+    WriteByte(Sys3+0x1670,rngMagicCost) -- Blizzaga Cost
+    WriteByte(Sys3+0xA10,rngMagicCost) -- Thunder Cost
+    WriteByte(Sys3+0x16A0,rngMagicCost) -- Thundara Cost
+    WriteByte(Sys3+0x16D0,rngMagicCost) -- Thundaga Cost
+    WriteByte(Sys3+0xA70,rngMagicCost) -- Cure Cost
+    WriteByte(Sys3+0x1700,rngMagicCost) -- Cura Cost
+    WriteByte(Sys3+0x1730,rngMagicCost) -- Curaga Cost
+    WriteByte(Sys3+0x1F40,rngMagicCost) -- Magnet Cost
+    WriteByte(Sys3+0x1F70,rngMagicCost) -- Magnera Cost
+    WriteByte(Sys3+0x1FA0,rngMagicCost) -- Magnega Cost
+    WriteByte(Sys3+0x1FD0,rngMagicCost) -- Reflect Cost
+    WriteByte(Sys3+0x2000,rngMagicCost) -- Reflera Cost
+    WriteByte(Sys3+0x2030,rngMagicCost) -- Reflega Cost
+    WriteByte(Sys3+0xE30,rngMagicCost) -- Twin Howl Cost
+    WriteByte(Sys3+0xFB0,rngMagicCost) -- Bushido Cost
+    WriteByte(Sys3+0x1940,rngMagicCost) -- Red Rocket Cost
+    WriteByte(Sys3+0x3F80,rngMagicCost) -- Speedster Cost
+    WriteByte(Sys3+0x40A0,rngMagicCost) -- Bluff Cost
+    WriteByte(Sys3+0x4430,rngMagicCost) -- Wildcat Cost
+    WriteByte(Sys3+0x49A0,rngMagicCost) -- Dance Call Cost
+    WriteByte(Sys3+0x4B80,rngMagicCost) -- Setup Cost
+    WriteByte(Sys3+0x67D0,rngMagicCost) -- Session Cost
+    WriteByte(Sys3+0x5840,rngMagicCost) -- Trinity Limit Cost
+    WriteByte(Sys3+0x5840+0x1830,rngMagicCost) -- (Solo) Trinity Limit Cost
+    WriteByte(Sys3+0x2E10,rngMagicCost) -- Whirli-Goof Cost
+    WriteByte(Sys3+0x3D40,rngMagicCost) -- Knocksmash Cost
+    WriteByte(Sys3+0x3320,rngMagicCost) -- Comet Cost
+    WriteByte(Sys3+0x3E60,rngMagicCost) -- Duck Flare Cost
+    WriteByte(Sys3+0x7E50,rngMagicCost) -- Strike Raid Cost
+    WriteByte(Sys3+0x7D30,rngMagicCost) -- Sonic Blade Cost
+    WriteByte(Sys3+0x7C10,rngMagicCost) -- Ragnarok Cost
+    WriteByte(Sys3+0x7DC0,rngMagicCost) -- Ars Arcanum Cost
+    WriteFloat(0x250D322, rngQuickRun) -- QR1 Speed
+    WriteFloat(0x250D366, rngQuickRun) -- QR2 Speed
+    WriteFloat(0x250D3AA, rngQuickRun) -- QR3 Speed
+    WriteFloat(0x250D3EE, rngQuickRun) -- QR4 Speed
+    WriteFloat(0x250D432, rngQuickRun) -- QRAX2 Speed
+    WriteFloat(0x250D316, rngADodgeHeight) -- AD1 Height
+    WriteFloat(0x250D31A, rngADodgeSpeed) -- AD1 Speed
+    WriteFloat(0x250D35A, rngADodgeHeight) -- AD2 Height 
+    WriteFloat(0x250D35E, rngADodgeSpeed) -- AD2 Speed
+    WriteFloat(0x250D39E, rngADodgeHeight) -- AD3 Height
+    WriteFloat(0x250D3A3, rngADodgeSpeed) -- AD3 Speed
+    WriteFloat(0x250D3E2, rngADodgeHeight) -- AD4 Height
+    WriteFloat(0x250D3E6, rngADodgeSpeed) -- AD4 Speed
+    WriteFloat(0x250D426, rngADodgeHeight) -- ADAX2 Height
+    WriteFloat(0x250D42A, rngADodgeSpeed) -- ADAX2 Speed
     WriteFloat(soraJumpStrengthPointer, rngHighJump, true)
     if ReadShort(Now+0) == 0x1C12 and ReadShort(Now+8) == 0x44 then
         WriteFloat(soraScalePointer, 1, true)
     elseif ReadShort(Now+0) == 0x2202 and ReadShort(Now+8) == 0x9D then
         WriteFloat(soraScalePointer, 1, true)
-    else WriteFloat(soraScalePointer, rngScale, true)
+    else WriteFloat(soraScalePointer, rngScale, true) -- Base Jump Height
     end
-    if ReadShort(Now+0) == 0x0310 and ReadShort(Now+8) == 0x38 then -- Port Royal Barrels
+    -- Softlock prevention code with the scale function.
+    if ReadShort(Now+0) == 0x0310 and ReadShort(Now+8) == 0x38 then -- Port Royal Barrels (Not really a softlock but I included it anyway)
         WriteFloat(animpointer, 1, true)
     elseif ReadShort(Now+0) == 0x0505 and ReadShort(Now+8) == 0x4F then -- Dark Thorn
         WriteFloat(animpointer, 1, true)
@@ -203,10 +222,12 @@ function Cheats()
         WriteFloat(animpointer, 1, true)
     elseif ReadShort(Now+0) == 0x2202 and ReadShort(Now+8) == 0x9D then -- Twilight Thorn
         WriteFloat(animpointer, 1, true)
-    elseif ReadShort(Now+0) == 0x1A12 and ReadShort(Now+8) == 0x45 then -- TWTNW Outside Core
+    elseif ReadShort(Now+0) == 0x1A12 and ReadShort(Now+8) == 0x45 then -- TWTNW Cylinders Battle
         WriteFloat(animpointer, 1, true)
     else WriteFloat(animpointer, rngAnimation, true)
     end
+    -- These are what the file will print to the console [F2] with your new stats. If you would like to add your own, just follow the format!
+    -- You may also safely remove any prints by deleting them or commenting them out with double dashes.
     if rngWait == 0 then
         ConsolePrint("New Attack:")
         ConsolePrint(rngAttack)
@@ -237,11 +258,13 @@ function Cheats()
         ConsolePrint("All Spells and Limits Now Cost:")
         ConsolePrint(rngMagicCost)
     end
-    if rngWait == 180 then
+    -- Optional warnings to notify when the next RNG event will happen.
+    -- Remember that the rngWait function counts down by 1 every frame!
+    if rngWait == 180 then -- 3 Seconds left
         ConsolePrint("Next random set of stats in 3 seconds!")
-    elseif rngWait == 120 then
+    elseif rngWait == 120 then -- 2 Seconds left
         ConsolePrint("Next random set of stats in 2 seconds!")
-    elseif rngWait == 60 then
+    elseif rngWait == 60 then -- 1 Second left
         ConsolePrint("Next random set of stats in 1 second!")
     elseif rngWait == 0 then
         rngWait = 3600
